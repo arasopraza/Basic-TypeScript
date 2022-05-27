ab44c8662e61eb1b566a47ac1bfb4441e1c614b8
arasopraza <aras.opraza@gmail.com>
2022-05-27 10:09:42 +0700

    TypeScript Array

--- object.ts
+++ object.ts
@@ -1,6 +1,14 @@
 const person = {
   name: 'Aras',
-  age: 21
+  age: 21,
+  hobbies: ['Sport', 'Reading']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Reading'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
