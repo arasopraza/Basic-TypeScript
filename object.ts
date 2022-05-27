975d68bdc01862e1af5a13f83276c2c715e5c10a
arasopraza <aras.opraza@gmail.com>
2022-05-27 10:16:38 +0700

    TypeScript tuple

--- object.ts
+++ object.ts
@@ -1,7 +1,13 @@
-const person = {
+const person: {
+  name: string,
+  age: number,
+  hobbies: string[],
+  role: [number, string]
+} = {
   name: 'Aras',
   age: 21,
-  hobbies: ['Sport', 'Reading']
+  hobbies: ['Sport', 'Reading'],
+  role: [2, 'Admin']
 };
 
 let favoriteActivities: string[];
