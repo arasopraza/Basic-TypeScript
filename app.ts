ec8e859c008c6fee2c8418af54067e77800dd2b4
arasopraza <aras.opraza@gmail.com>
2022-05-27 22:14:51 +0700

    TypeScript litral type

--- app.ts
+++ app.ts
@@ -1,15 +1,18 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
   let result
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combineAges = combine(10, 12);
+const combineAges = combine(10, 12, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine("aras", "opraza");
+const combineAgesString = combine('10', '12', 'as-number');
+console.log(combineAges);
+
+const combineNames = combine("aras", "opraza", 'as-text');
 console.log(combineNames);
