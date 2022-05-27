e487a1d98c1233c8d9ebdf40b63090105127cc92
arasopraza <aras.opraza@gmail.com>
2022-05-27 22:20:30 +0700

    TypeScript type aliases

---
+++ literal-type.ts
@@ -0,0 +1,18 @@
+// function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
+//   let result
+//   if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+//     result = +input1 + +input2;
+//   } else {
+//     result = input1.toString() + input2.toString();
+//   }
+//   return result;
+// }
+
+// const combineAges = combine(10, 12, 'as-number');
+// console.log(combineAges);
+
+// const combineAgesString = combine('10', '12', 'as-number');
+// console.log(combineAges);
+
+// const combineNames = combine("aras", "opraza", 'as-text');
+// console.log(combineNames);
