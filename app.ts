ac757eab92bde7d2bbce1fcc69aa20e96409b674
arasopraza <aras.opraza@gmail.com>
2022-05-27 10:00:05 +0700

    TypeScript inference

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, pharase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(pharase + result);
+  } else {
+    return result
+  }
 }
 
 const numberOne = 5;
 const numberTwo = 28;
+const printResult = true;
+const resultPharase = 'Result is : ';
 
-const resultAdd = add(numberOne, numberTwo);
-console.log(resultAdd);
+add(numberOne, numberTwo, printResult, resultPharase);
