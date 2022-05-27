c78d01bbdafa85ba1b0df60ddaf35211b88d7900
arasopraza <aras.opraza@gmail.com>
2022-05-27 10:26:07 +0700

    TypeScript enum

--- object.ts
+++ object.ts
@@ -1,14 +1,23 @@
-const person: {
-  name: string,
-  age: number,
-  hobbies: string[],
-  role: [number, string]
-} = {
+// const person: {
+//   name: string,
+//   age: number,
+//   hobbies: string[],
+//   role: [number, string]
+// } = {
+//   name: 'Aras',
+//   age: 21,
+//   hobbies: ['Sport', 'Reading'],
+//   role: [2, 'Admin']
+// };
+
+enum Role { ADMIN, READ_ONLY, AUTHOR};
+
+const person = {
   name: 'Aras',
   age: 21,
   hobbies: ['Sport', 'Reading'],
-  role: [2, 'Admin']
-};
+  role: Role.AUTHOR
+}
 
 let favoriteActivities: string[];
 favoriteActivities = ['Reading'];
@@ -17,4 +26,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role == Role.AUTHOR) {
+  console.log('is author');
 }
