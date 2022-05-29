d80f8ca6bfbc40884024509b5725a7a06b2f13b8
arasopraza <aras.opraza@gmail.com>
2022-05-29 15:24:43 +0700

    TypeScript never type

--- app.ts
+++ app.ts
@@ -1,8 +1,5 @@
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = "aras";
-if (typeof userInput === 'string'){
-  userName = userInput;
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
+
+generateError('An error occured', 500);
---
+++ unknown-type.ts
@@ -0,0 +1,8 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "aras";
+if (typeof userInput === 'string'){
+  userName = userInput;
+}
