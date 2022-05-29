491d55c63dd0206f2747e3b03e61ea2a14796ed2
arasopraza <aras.opraza@gmail.com>
2022-05-29 15:05:40 +0700

    TypeScript unknown type

--- app.ts
+++ app.ts
@@ -1,24 +1,8 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "aras";
+if (typeof userInput === 'string'){
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log('Result: ' + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
