1f4397d3ba0ff1747d1f46861ff0772ffae6de89
arasopraza <aras.opraza@gmail.com>
2022-05-29 14:39:35 +0700

    Function return and function type

--- app.ts
+++ app.ts
@@ -7,3 +7,9 @@ function printResult(num: number): void {
 }
 
 printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
